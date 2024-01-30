c6d48a63e88eabb1e1cf095febc2e1e650040c59
Omarmoatz <omar.raouf.002@gmail.com>
2024-01-30 17:30:29 +0200

    design movies models

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+SHOW_CHOICES = (
+    ('Movie','Movie'),
+    ('Series','Series'),
+    ('Theater','Theater'),
+    ('Animation','Animation'),
+)
+
+class Show(models.Model):
+    user = models.ForeignKey(User, related_name='user_show', on_delete=models.CASCADE)
+    title = models.CharField(max_length=100,default='default_show', blank=True)
+    description = models.TextField(max_length=5000,default='default_description', blank=True)
+    image = models.ImageField( upload_to='show/',blank=True, null=True)
+    rating = models.DecimalField( max_digits=4, decimal_places=2)
+    type = models.CharField(max_length=50,choices=SHOW_CHOICES, blank=True)
+    category = models.ForeignKey('Category', related_name='category_show', on_delete=models.SET_DEFAULT,default='default_ctg')
+    main_actor = models.ForeignKey('Actor', related_name='mainActor_show', on_delete=models.SET_DEFAULT,default='somebody')
+    sub_actor = models.ForeignKey('Actor', related_name='subActor_show', on_delete=models.SET_DEFAULT,default='somebody2')
+
+    def __str__(self):
+        return self.title
+
+
+ACTOR_GENDER = (
+    ('Male','Male'),
+    ('Female','Female'),
+)
+class Actor(models.Model):
+    name = models.CharField( max_length=100, default='somebody', blank=True)
+    age = models.PositiveIntegerField( default=20, blank=True)
+    gender = models.CharField( max_length=50, choices=ACTOR_GENDER, blank=True)
+    image = models.ImageField( upload_to='actor/',blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    title = models.CharField( max_length=100, default='default_ctg', blank=True)
+    image = models.ImageField( upload_to='category/',blank=True, null=True)
+
+    def __str__(self):
+        return self.title
--- project/settings.py
+++ project/settings.py
@@ -31,12 +31,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # default apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 3rd party apps
+
+
+    # my apps
+    'movies',
 ]
 
 MIDDLEWARE = [
