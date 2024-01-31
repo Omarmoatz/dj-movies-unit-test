7de9fa45f34568b75bd974b9c67104837019a7fa
Omarmoatz <omar.raouf.002@gmail.com>
2024-01-31 16:02:28 +0200

    used viewset api

--- movies/urls.py
+++ movies/urls.py
@@ -1,9 +1,17 @@
-from django.urls import path
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+
 from . import views
+
+router = DefaultRouter()
+router.register('',views.ShowViewSets)
+
 
 app_name = 'movies'
 
 urlpatterns = [
+    path('viewSet/', include(router.urls)),
+    
     path('list/', views.shows_list),
     path('detail/<int:pk>/', views.shows_detail),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
+from rest_framework import status,viewsets
 
 from .models import Show
 from .serializers import ShowSerializer,ShowDetailSerializer
@@ -10,10 +11,21 @@ from .serializers import ShowSerializer,ShowDetailSerializer
 def shows_list(request):
     show = Show.objects.all()
     serializer = ShowSerializer(show,many=True).data
-    return Response({'data':serializer})
+    return Response({'data':serializer}
+                    ,status=status.HTTP_200_OK)
 
 @api_view(['GET'])
 def shows_detail(request,pk):
     show = get_object_or_404(Show,id=pk)
     serializer = ShowDetailSerializer(show).data
-    return Response({'data':serializer})
+    return Response({'data':serializer}
+                    ,status=status.HTTP_200_OK)
+
+class ShowViewSets(viewsets.ViewSet):
+    queryset = Show.objects.all()
+    serializer_class = ShowSerializer
+    
+    # def list(self):
+    #     serializer = ShowSerializer(self.queryset,many=True).data
+    #     return Response({'data':serializer}
+    #                 ,status=status.HTTP_200_OK)
